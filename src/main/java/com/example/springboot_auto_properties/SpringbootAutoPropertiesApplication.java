3981a24ddc34cef083aba6425cecb58788487f07
Gonçalo Ribeiro <goncalocribeiro@tecnico.ulisboa.pt>
2022-03-07 16:43:59 +0000

    init springboot project

---
+++ src/main/java/com/example/springboot_auto_properties/SpringbootAutoPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_auto_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutoPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAutoPropertiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_auto_properties/SpringbootAutoPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_auto_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAutoPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
